27c6a5c4d25d43a76a0700d2f814408345dbfc3a
Marvin-Kabugi <kabugijeff@gmail.com>
2023-05-21 12:38:48 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt") -> None:
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Woof!")
+
+    def showing_self(self):
+        return self
+    
+    def adopt(self, owner_name):
+        self.owner = owner_name
+
+
+# fido = Dog("Fido")
+# fido.name
+
+# snoopy = Dog("Snoopy")
+# snoopy.name
